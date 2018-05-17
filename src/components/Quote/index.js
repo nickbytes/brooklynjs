b7396d6f9299e0125d401867330c6c4802fdeb1d
Nick Beattie <nick@nickbytes.com>
2018-05-17 14:12:15 -0400

    feat; styling

--- src/components/Quote/index.js
+++ src/components/Quote/index.js
@@ -4,9 +4,10 @@ import styled from "styled-components";
 const Blockquote = styled.blockquote`
   font-size: 35px;
   line-height: 42px;
-  margin-bottom: 60px;
+  margin: 0 0 60px;
+  padding: 40px;
   font-weight: 400;
-  background-color: #87cc74;
+  background-color: rgba(170, 177, 193, 0.5);
 `;
 
 const Quote = ({ children }) => <Blockquote>{children}</Blockquote>;
